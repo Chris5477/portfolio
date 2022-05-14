ab6967a57f305ebd65315f92b683a58d62098081
Chris5477 <petitcolaschristopher@gmail.com>
2022-05-14 17:09:15 +0200

    add content home page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 import HomePage from "./page/HomePage";
-import ProjectsPage from "./page/ProjectsPage"
-import AboutPage from "./page/AboutPage"
+import PortfolioPage from "./page/PortfolioPage";
+import AboutPage from "./page/AboutPage";
 import ErrorPage from "./page/ErrorPage";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import TopNavigation from "./components/TopNavigation";
 
 function App() {
-  return (
-    <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/projects" element={<ProjectsPage />} />
-          <Route path="/about" element={<AboutPage />} />
-          <Route path="*" element={<ErrorPage />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
-  );
+	return (
+		<div className="App">
+			<BrowserRouter>
+      <TopNavigation />
+				<Routes>
+					<Route path="/" element={<HomePage />} />
+					<Route path="/portfolio" element={<PortfolioPage />} />
+					<Route path="/about" element={<AboutPage />} />
+					<Route path="*" element={<ErrorPage />} />
+				</Routes>
+			</BrowserRouter>
+		</div>
+	);
 }
 
 export default App;
