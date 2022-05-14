809b24bd95fc6ea5945789a396830103140f81dd
Chris5477 <petitcolaschristopher@gmail.com>
2022-05-14 15:12:35 +0200

    add mui and react-router dom dependencies and create router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import HomePage from "./page/HomePage";
+import ProjectsPage from "./page/ProjectsPage"
+import AboutPage from "./page/AboutPage"
+import ErrorPage from "./page/ErrorPage";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/projects" element={<ProjectsPage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="*" element={<ErrorPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
