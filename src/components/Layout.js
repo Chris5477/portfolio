277e7b5cba5998a83b5e8a224b3749cf0e022a65
Chris5477 <petitcolaschristopher@gmail.com>
2022-05-15 17:08:40 +0200

    add style wip

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,13 +2,13 @@ import { Paper } from "@mui/material";
 import { theme } from "../utils/theme";
 import { ThemeProvider } from "@mui/material/styles";
 import TopNavigation from "./TopNavigation";
-import bg from "../assets/pictures/bg-project.png";
+
 
 const Layout = ({ page }) => {
 	console.log(theme);
 	return (
 		<ThemeProvider theme={theme}>
-			<Paper sx={{ width: "100%", height: "100vh", backgroundImage: `url(${bg})` }}>
+			<Paper sx={{ width: "100%", height:"auto", backgroundColor:theme.palette.primary.main }}>
 				<TopNavigation />
 				{page}
 			</Paper>
--- src/page/PortfolioPage.js
+++ src/page/PortfolioPage.js
@@ -1,23 +1,32 @@
-import { Paper, Typography } from "@mui/material";
+import { Grid, Typography } from "@mui/material";
 import CardProject from "../components/CardProject";
 import { projects } from "../utils/listProject";
 
 const ProjectsPage = () => {
 	return (
-		<Paper>
-			<Typography variant="h1">Mon portfolio</Typography>
-			<Typography paragraph>
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda optio rem quia omnis, dolorum, reiciendis ad
-				expedita exercitationem laudantium, dignissimos vero laborum autem! Ducimus consequatur omnis a ex doloribus
-				vero. Repudiandae delectus numquam sequi quia, qui fugiat impedit. Eum sapiente, laudantium cupiditate
-				accusantium repellendus alias unde quia hic est assumenda eos, rerum, quam quaerat pariatur dolorum. Illo
-				ducimus necessitatibus molestias!
-			</Typography>
-			Description project
+		<Grid container width={"100%"} minHeight={"100vh"} height="auto" spacing={2}>
+			<Grid mt={10} ml={5} item xs={12}>
+				<Typography variant="h1" color={"secondary.light"} fontWeight={500} >Mon portfolio</Typography>
+				<Typography paragraph fontSize={25} color={"background.default"}>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda optio rem quia omnis, dolorum, reiciendis
+					ad expedita exercitationem laudantium, dignissimos vero laborum autem! Ducimus consequatur omnis a ex
+					doloribus vero.
+				</Typography>
+			</Grid>	
+			
 			{projects.map(({ id, name, description, illustrations, github }) => (
-				<CardProject key={id} picture={illustrations[0]} title={name} description={description} link={github} more={`/portfolio/project/${name}`} />
+				<Grid item xs={12} md={3}>
+				<CardProject
+					key={id}
+					picture={illustrations[0]}
+					title={name}
+					description={description}
+					link={github}
+					more={`/portfolio/project/${name}`}
+				/>
+				</Grid>
 			))}
-		</Paper>
+		</Grid>
 	);
 };
 export default ProjectsPage;
