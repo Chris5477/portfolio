0dcf5a55ef3f050120faa92534f12adba5ddc326
Chris5477 <petitcolaschristopher@gmail.com>
2022-05-16 20:47:25 +0200

    add gh-pages and remove utils repo in gitignore

---
+++ src/utils/listsLinks.js
@@ -0,0 +1,45 @@
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import AttachmentIcon from "@mui/icons-material/Attachment";
+import LanguageIcon from "@mui/icons-material/Language";
+import ContactMailIcon from "@mui/icons-material/ContactMail";
+import fileSaver from "file-saver";
+
+const saveFile = () => {
+	fileSaver.saveAs(
+		process.env.PUBLIC_URL + "./assets/CV_ChristopherPetitcolas_Alternance_DEVWEB.pdf",
+		"CV _ Christopher Petitcolas_Alternance_DEVWEB	.pdf"
+	);
+};
+
+export const listLinks = [
+	{
+		id: 1,
+		icon: <LinkedInIcon sx={{ mr: 3 }} />,
+		title: "Linkedin",
+		url: "https://www.linkedin.com/in/christopher-petitcolas/",
+	},
+	{
+		id: 2,
+		icon: <GitHubIcon sx={{ mr: 3 }} />,
+		title: "Github",
+		url: "https://github.com/Chris5477",
+	},
+	{
+		id: 3,
+		icon: <LanguageIcon sx={{ mr: 3 }} />,
+		title: "WeLovesDevs",
+		url: "https://christopherpetitcola.welovedevs.com",
+	},
+	{
+		id: 4,
+		icon: <AttachmentIcon sx={{ mr: 3 }} />,
+		title: "Télécharger mon C-V",
+		handleClick: saveFile,
+	},
+	{
+		id: 5,
+		icon: <ContactMailIcon sx={{ mr: 3 }} />,
+		title: "Contactez-moi",
+	},
+];
---
+++ src/utils/presentation.js
@@ -0,0 +1,42 @@
+export const presentation = [
+	{
+		id: 1,
+		title: "Presentation",
+		description: `Je m'appelle Christopher, j'ai 31 ans et je suis jeune diplômé d'une formation de développeur Front-End chez Openclassrooms. J'ai travaillé pendant une dizaine d'année dans la grande distribution sur plusieurs postes. Le manque de perspective d'évolution m'a motivé à m'orienter vers une reconversion professionnelle. J'ai choisi le développement web, car j'ai gardé un très bon souvenir de mon grand frère qui m'a initié au HTML dans mon enfance. J'ai choisi une formation de développeur Front-End pour la satisfaction de pouvoir visualiser le code écrit sur un navigateur. Par curiosité et de pouvoir conserver des données, j'ai décidé d'apprendre Node js et la base de données MongoDB pour parvenir a créer une application complète.`,
+	},
+	{
+		id: 2,
+		title: "Ma recherche d'entreprise",
+		description: `Je recherche ma première expérience professionnelle en tant que développeur Front-End, en CDI, CDD ou encore en contrat d'apprentissage. Le rythme de mon contrat d'apprentissage est de 4 jours en entreprise et d'un jour d'école par semaine et ce pendant 1 an. Dans l'idéal, je souhaite trouver un grand groupe pour pouvoir monter rapidement en compétences. En ce qui concerne le mode de travail, travailler en présentiel serait un atout pour la proximité avec les collègues. Je suis mobile dans toute la France. J'ai une préférence pour la région Grand Est vers la ville de Nancy pour me rapprocher de ma famille. Ou encore dans le sud de la France, comme Aix-en-Provence par exemple. Pour la rémunération, un contrat en CDI ou CDD, le marché du travail indique que le salaire d'un développeur Front-End junior est de 27 K€ annuel brut. Pour le contrat d'apprentissage, le salaire est à discuter. De plus, je suis éligible à la prime de 8 000 € offerte par l'État pour encourager les entreprises à recruter des alternants.`,
+	},
+	{
+		id: 3,
+		title: "Formation",
+		description: `Diplomé d'une formation de développeur front-end chez OpenClassrooms. Cette expérience m'a appris à être persévérant face aux difficultés, d'apprendre en autonomie, et d'organiser mon temps pour réaliser les différents projets. J'ai également eu l’opportunité de présenter des livrables lors de soutenance afin de valider mes projets, ce qui m’a permis de gérer le stress et de bien progresser sur mes présentations orales. Grâce à cette formation, je suis capable de concevoir des maquettes stylisées et dynamique avec HTML5, CSS3, Sass, Javascript, en respectant les normes W3C. J'ai découvert la librairie React.js pour créer des applications single-page, avec les notions de cycle de vie de composant, de state, et de props. Pour gérer les états au sein de l'application, j'ai également eu l'occasion de travailler avec Redux. J'ai pu travailler avec des API notemment les requêtes CRUD, ce qui a attirer ma curiosité sur la création d'API et de serveur. J'ai donc pu acquérir des petites connaissances sur node.js express et mongoDB. `,
+	},
+	{
+		id: 4,
+		title: "Expérience professionnelle",
+		description: `Lors de ma dernière expérience professionnelle. J'ai eu la chance d’inaugurer le service drive. J'ai eu l'occasion de développer des compétences transversales comme, apprendre à travailler en équipe. En effet en tant que préparateur de commande, nous devons communiquer pour gérer les tâches. J'ai eu l'occasion de manager une petite équipe d'étudiant les week-ends, et d'apprendre a déléguer le travail. En tant que préparateur de commande, il faut aussi savoir organiser son temps pour respecter les créneaux horaires des commandes. J'ai également su adapter les méthodes de travail pour gagner en productivité. `,
+	},
+	{
+		id: 5,
+		title: "Interêts",
+		description: `Avoir l'opportunité de rejoindre une entreprise qui œuvre sur des projets innovants et de pouvoir travailler sur des solutions qui répondent aux besoins des utilisateurs serait une très bonne chose. 
+		En effet, je suis passionné, des nouvelles technologies, du digitale ou encore de l'intelligence artificielle.
+		Par conséquent, pouvoir lier mon activité professionnelle pour aider des personnes dans le domaine médical, développer des applications pour aider au respect de notre environnement ou encore pour faciliter notre quotidien serai une excellente raison de me lever le matin.`,
+	},
+	{
+		id: 6,
+		title: "Mes Hobbies",
+		description: `Étant né dans les années 90, j'ai pu grandir avec le jeu vidéo, et Final fantasy 7 est celui qui m'a passionnée pour cet art. J'aime tous les types de jeu, mais les FPS ou les jeux d'aventure post-apocalyptique sont mes préférées. The last of us est ma licence préférée. C'est d'ailleurs cette licence qui attire toute ma curiosité sur l'intelligence artificielle, car elle est plutôt bien développée. 
+		J'aime également faire de la guitare, je possède deux guitares acoustiques et une électrique. Jouer de la guitare m'apporte une échappatoire afin de me vider l'esprit. En plus réussir à jouer un morceau qu'on apprécie est satisfaisant.
+		La cuisine est quelque chose qui me plaît. Si de beau matin, on me dit ce soir, on fait un barbecue ? Ma journée serait géniale quoiqu'il arrive. Cuisiner des choses qu'on aime est géniale, ça développe la créativité ou encore, on peut tenter des mélanges improbables. `,
+	},
+	{
+		id: 7	,
+		title: "Mes objectifs",
+		description: `Dans un premier temps, mon premier objectif est d'avoir une première expérience professionnelle réussie en tant que développeur web. Ensuite, j'aimerais pouvoir me former au métier de développeur Full-Stack ou associer mes compétences de développeur au domaine de l'intelligence artificielle. Puis en fin de carrière, j'aimerais utiliser tout ce que m'a offert ma carrière pour créer une entreprise aux États-Unis`,
+	},
+	
+];
