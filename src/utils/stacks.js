a6b17e676592d5bfc83728e9e4c000dddd50bdcd
Chris5477 <petitcolaschristopher@gmail.com>
2022-05-16 20:47:25 +0200

    add gh-pages and remove utils repo in gitignore

---
+++ src/utils/stacks.js
@@ -0,0 +1,77 @@
+import html from "../assets/icons/html.png";
+import css from "../assets/icons/css.png";
+import sass from "../assets/icons/sass.svg";
+import javascript from "../assets/icons/javascript.png";
+import react from "../assets/icons/react.svg";
+import redux from "../assets/icons/redux.svg";
+import jest from "../assets/icons/jest.png";
+import mongo from "../assets/icons/mongo-db.png";
+import figma from "../assets/icons/figma.png";
+import node from "../assets/icons/node.svg"
+import psql from "../assets/icons/psql.svg"
+import git from "../assets/icons/git.svg"
+import typescript from "../assets/icons/typescript.svg"
+import webGl from "../assets/icons/webGl.svg"
+
+
+export const skills = [
+	{
+		id: 1,
+		icon: html,
+	},
+	{
+		id: 2,
+		icon: css,
+	},
+	{
+		id: 3,
+		icon: sass,
+	},
+	{
+		id: 4,
+		icon: javascript,
+	},
+	{
+		id: 5,
+		icon: react,
+	},
+	{
+		id: 6,
+		icon: redux,
+	},
+	{
+		id: 7,
+		icon: jest,
+	},
+];
+
+export const knowledge =  [
+    {
+		id: 1,
+		icon: node,
+	},
+	{
+        id: 2,
+		icon: mongo,
+	},
+	{
+        id: 3,
+		icon: typescript,
+	},
+	{
+		id: 4,
+		icon: webGl,
+	},
+	{
+        id: 5,
+		icon: psql,
+	},
+	{
+		id: 6,
+		icon: git,
+	},
+    {
+        id: 7,
+        icon: figma,
+    },
+]
