ec2f55d124f2f3371c3cef7553d2e7e6acbdaf70
Chris5477 <petitcolaschristopher@gmail.com>
2022-05-19 11:20:00 +0200

    new try to fix router problem

--- src/App.js
+++ src/App.js
@@ -2,21 +2,21 @@ import HomePage from "./page/HomePage";
 import PortfolioPage from "./page/PortfolioPage";
 import AboutPage from "./page/AboutPage";
 import ErrorPage from "./page/ErrorPage";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout";
 import "./app.css";
 
 function App() {
 	return (
 		<div className="App">
-			<BrowserRouter basename={process.env.PUBLIC_URL}>
+			<HashRouter basename={process.env.PUBLIC_URL}>
 				<Routes>
 					<Route exact path="/" element={<Layout page={<HomePage />} />} />
 					<Route path="/myportfolio" element={<Layout page={<PortfolioPage />} />} />
 					<Route path="/about" element={<Layout page={<AboutPage />} />} />
 					<Route path="*" element={<ErrorPage />} />
 				</Routes>
-			</BrowserRouter>
+			</HashRouter>
 		</div>
 	);
 }
