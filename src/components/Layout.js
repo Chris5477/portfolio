3baafbd47ace4c260331e329d94cfc9aa25d97fa
Chris5477 <petitcolaschristopher@gmail.com>
2022-05-19 11:20:00 +0200

    new try to fix router problem

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-import TopNavigation from "./TopNavigation"
-
-const Layout = () => {
-return(
-   <div className="layout">
-       <TopNavigation />
-       <div></div>
-       <div></div>
-   </div> 
-)
-}
-export default Layout
-=======
 import { Paper } from "@mui/material";
 import { theme } from "../utils/theme";
 import { ThemeProvider } from "@mui/material/styles";
@@ -28,4 +14,3 @@ const Layout = ({ page }) => {
 	);
 };
 export default Layout;
->>>>>>> add style navigation bar and home page
