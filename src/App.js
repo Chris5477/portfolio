0aab72352d80f90e0982b787bff34757cf3c7e6c
Chris5477 <petitcolaschristopher@gmail.com>
2022-05-19 11:33:57 +0200

    new try fix router

--- src/App.js
+++ src/App.js
@@ -2,21 +2,39 @@ import HomePage from "./page/HomePage";
 import PortfolioPage from "./page/PortfolioPage";
 import AboutPage from "./page/AboutPage";
 import ErrorPage from "./page/ErrorPage";
-import { HashRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout";
 import "./app.css";
 
 function App() {
+
+	const urlLocal = {
+		home : "portfolio/",
+		portfolio : "portfolio/myportfolio",
+		about : "portfolio/about"
+	}
+
+	const urlDistant = {
+		home : "https://chris5477.github.io/portfolio/",
+		portfolio : "https://chris5477.github.io/portfolio/myportfolio",
+		about : "https://chris5477.github.io/portfolio/about"
+	}
+
+
+
+	const isLocalDev = window.location.host == "localhost:3000" ? urlLocal : urlDistant
+
+	console.log(isLocalDev)
 	return (
 		<div className="App">
-			<HashRouter basename={process.env.PUBLIC_URL}>
+			<BrowserRouter >
 				<Routes>
-					<Route exact path="/" element={<Layout page={<HomePage />} />} />
-					<Route path="/myportfolio" element={<Layout page={<PortfolioPage />} />} />
-					<Route path="/about" element={<Layout page={<AboutPage />} />} />
+					<Route exact path={isLocalDev.home} element={<Layout page={<HomePage />} />} />
+					<Route path={isLocalDev.portfolio}element={<Layout page={<PortfolioPage />} />} />
+					<Route path={isLocalDev.about} element={<Layout page={<AboutPage />} />} />
 					<Route path="*" element={<ErrorPage />} />
 				</Routes>
-			</HashRouter>
+			</BrowserRouter>
 		</div>
 	);
 }
